59c3b5ff4d15fd020da3bd4f533c3f5af611df2c
alexseymourdev <alexseymourdev@gmail.com>
2023-04-20 10:16:11 +0100

    adding chart helpers

---
+++ projects/diu-component-library/src/lib/_functions/helper_charts.ts
@@ -0,0 +1,93 @@
+import * as helpers from "chart.js/helpers";
+
+export const createTooltip = (context) => {
+    const { chart, tooltip } = context;
+    let tooltipEl = chart.canvas.parentNode.querySelector("div");
+
+    if (!tooltipEl) {
+        // Create tooltip
+        tooltipEl = document.createElement("div");
+        tooltipEl.style.pointerEvents = "none";
+        tooltipEl.style.position = "absolute";
+        tooltipEl.style["z-index"] = "100";
+        tooltipEl.style.transform = "translate(-50%, -50%)";
+        tooltipEl.style.transition = "all .1s ease";
+        tooltipEl.style.overflow = "auto";
+        tooltipEl.style["border-radius"] = "15px";
+
+        // Create content div
+        const bodyDiv = document.createElement("DIV");
+        bodyDiv.classList.add("body");
+        tooltipEl.appendChild(bodyDiv);
+
+        // Add to chart
+        chart.canvas.parentNode.appendChild(tooltipEl);
+    }
+
+    // Set position
+    tooltipEl.style.top = tooltip.y + "px";
+    return tooltipEl;
+}
+
+export const barChartValues = function(chart) {
+    const ctx = chart.ctx;
+    ctx.textAlign = "left";
+    helpers.each(chart.data.datasets.forEach((dataset, i) => {
+        const meta = chart.getDatasetMeta(i);
+        helpers.each(meta.data.forEach((bar, index) => {
+            ctx.fillText(dataset.data[index], bar.base + 5, bar.y + 4);
+        }),this)
+    }),this);
+}
+
+export const barChartLabels = function(chart) {
+    const ctx = chart.ctx;
+    ctx.textAlign = "left";
+    helpers.each(chart.data.labels.forEach((label, i) => {
+        const meta = chart.getDatasetMeta(i);
+        helpers.each(meta.data.forEach((bar, index) => {
+            const text = chart.data.labels[index];
+
+            // Add background
+            if(chart?.config?.options?.plugins?.barChartLabels?.backdropColor) {
+                const padding = 5;
+                const width = ctx.measureText(text).width;
+                ctx.fillStyle = chart?.config?.options?.plugins?.barChartLabels?.backdropColor;
+                ctx.fillRect(
+                    bar.base + 5 - (padding / 2),
+                    bar.y - 5 - (padding / 2),
+                    width + padding,
+                    10 + padding,
+                );
+            }
+
+            // Write text
+            ctx.fillStyle = "#000";
+            ctx.fillText(text, bar.base + 5, bar.y + 4);
+        }),this)
+    }),this);
+}
+
+export const matrixChartLabels = function(chart) {
+    const ctx = chart.ctx;
+    ctx.textAlign = "left";
+    helpers.each(chart.data.datasets.forEach((dataset, i) => {
+        const meta = chart.getDatasetMeta(i);
+        helpers.each(meta.data.forEach((bar: any, index) => {
+            ctx.textAlign = "center";
+            ctx.fillText(
+                numberWithCommas(bar.$context.raw.v),
+                bar.x + (bar.width / 2),
+                bar.y + (bar.height / 2)
+            );
+        }),this)
+    }),this);
+}
+
+export const numberWithCommas = (x): string => {
+    if (x === null || typeof x === "undefined") {
+        return "Nothing selected";
+    } else {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+}
