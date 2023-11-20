3d727658c836f0b2c60fdd5b32dd34b6f33b0633
luket-nhs <luke.tinnion@nhs.net>
2023-11-20 17:01:01 +0000

    Final commit

--- projects/diu-component-library/src/lib/_components/dashboard-population/dashboard-population.component.ts
+++ projects/diu-component-library/src/lib/_components/dashboard-population/dashboard-population.component.ts
@@ -1,37 +1,11 @@
-import { Component, ViewChild, Inject, AfterViewInit, ViewContainerRef } from "@angular/core";
-import { PopulationMapNewComponent } from "./new/dashboard-population.component";
-import { PopulationMapLegacyComponent } from "./legacy/dashboard-population.component";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "app-population-dashboard",
     templateUrl: "./dashboard-population.component.html",
     animations: [],
 })
-export class DashboardPopulationComponent implements AfterViewInit {
+export class DashboardPopulationComponent {
 
-    version;
-    @ViewChild("mapContainer", { read: ViewContainerRef }) mapContainerElement: ViewContainerRef;
-
-    constructor(
-        @Inject("environment") private environment
-    ) {}
-
-    ngAfterViewInit() {
-        // Initially set version depending on environment
-        this.setVersion(this.environment.production === true ? "legacy" : "new");
-    }
-
-    setVersion(version) {
-        // Set version
-        this.version = version;
-
-        // Add component
-        this.mapContainerElement.clear();
-        this.mapContainerElement.createComponent<any>(
-            {
-                legacy: PopulationMapLegacyComponent,
-                new: PopulationMapNewComponent
-            }[this.version]
-        );
-    }
+    constructor() {}
 }
